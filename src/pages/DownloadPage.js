9ed9022070f7320bd4622df2a1be15c936524567
ranjian0 <karanjaichungwa@gmail.com>
2020-08-29 16:42:27 +0300

    implement doanload page

--- src/pages/DownloadPage.js
+++ src/pages/DownloadPage.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { List, Typography } from 'antd'
+
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+];
 
 const DownloadPage = () => {
     return (
         <div>
-            <p> Downloads </p>
+            <List
+              header={<Typography.Text strong>Stable Releases</Typography.Text>}
+              bordered
+              dataSource={data}
+              renderItem={item => (
+                <List.Item>
+                  <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                </List.Item>
+              )}
+            />
+
+            <br />
+
+            <List
+              header={<Typography.Text strong>Alpha Releases</Typography.Text>}
+              bordered
+              dataSource={data}
+              renderItem={item => (
+                <List.Item>
+                  <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                </List.Item>
+              )}
+            />
+
         </div>
     )
 }
