c6346728144726cab4058ccb5d0e9422b2974f17
ranjian0 <karanjaichungwa@gmail.com>
2020-08-29 16:42:38 +0300

    implement wiki page

--- src/pages/WikiPage.js
+++ src/pages/WikiPage.js
@@ -1,11 +1,65 @@
 import React from 'react';
+import { List, Card } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
+
+const data = [
+  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },
+  {
+    title: 'Title 5',
+  },
+  {
+    title: 'Title 6',
+  },
+];
+
 
 const WikiPage = () => {
     return (
-        <div>
-            <p> Wiki </p>
-        </div>
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 2,
+          md: 2,
+          lg: 2,
+          xl: 3,
+          xxl: 3,
+        }}
+        dataSource={data}
+        renderItem={item => (
+          <List.Item>
+            <Card
+                style={{ width: "300" }}
+                cover={
+                  <img
+                    alt="example"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                  />
+                }>
+
+                <Meta
+                  title={item.title}
+                  description="Short description for this"
+                />
+              </Card>
+            </List.Item>
+        )}
+      />
     )
 }
+
 
 export default WikiPage;
